d89e4c504c714f9972d8b6108c981e2e4b94dfd8
Jay <140453707+jaydxyz@users.noreply.github.com>
2024-06-24 15:45:09 -0700

    add mock data

--- app.js
+++ app.js
@@ -10,22 +10,75 @@ app.use(express.static('public'));
 
 // Mock data for component prices
 const componentPrices = {
-  'CPU001': [
-    { date: '2023-06-01', price: 150 },
-    { date: '2023-06-15', price: 155 },
-    { date: '2023-07-01', price: 148 },
-  ],
-  'RAM002': [
-    { date: '2023-06-01', price: 80 },
-    { date: '2023-06-15', price: 85 },
-    { date: '2023-07-01', price: 82 },
-  ],
-  'GPU003': [
-    { date: '2023-06-01', price: 300 },
-    { date: '2023-06-15', price: 310 },
-    { date: '2023-07-01', price: 305 },
-  ],
-};
+    'CPU001': [
+      { date: '2023-06-01', price: 150 },
+      { date: '2023-06-15', price: 155 },
+      { date: '2023-07-01', price: 148 },
+      { date: '2023-07-15', price: 152 },
+      { date: '2023-08-01', price: 149 },
+      { date: '2023-08-15', price: 153 }
+    ],
+    'RAM002': [
+      { date: '2023-06-01', price: 80 },
+      { date: '2023-06-15', price: 85 },
+      { date: '2023-07-01', price: 82 },
+      { date: '2023-07-15', price: 84 },
+      { date: '2023-08-01', price: 81 },
+      { date: '2023-08-15', price: 83 }
+    ],
+    'GPU003': [
+      { date: '2023-06-01', price: 300 },
+      { date: '2023-06-15', price: 310 },
+      { date: '2023-07-01', price: 305 },
+      { date: '2023-07-15', price: 315 },
+      { date: '2023-08-01', price: 308 },
+      { date: '2023-08-15', price: 312 }
+    ],
+    'SSD004': [
+      { date: '2023-06-01', price: 120 },
+      { date: '2023-06-15', price: 125 },
+      { date: '2023-07-01', price: 118 },
+      { date: '2023-07-15', price: 122 },
+      { date: '2023-08-01', price: 119 },
+      { date: '2023-08-15', price: 123 }
+    ],
+    'MB005': [
+      { date: '2023-06-01', price: 200 },
+      { date: '2023-06-15', price: 205 },
+      { date: '2023-07-01', price: 198 },
+      { date: '2023-07-15', p    { date: '2023-08-01', price: 199 },
+      { date: '2023-08-15', price: 203 }
+    ],
+    'PSU006': [
+      { date: '2023-06-01', price: 90 },
+      { date: '2023-06-15', price: 95 },
+      { date: '2023-07-01', price: 9    { date: '2023-07-15', price: 94 },
+      { date: '2023-08-01', price: 91 },
+      { date: '2023-08-15', price: 93 }
+    ],
+    'CASE007': [
+      { date: '2023-06-01', price: 70 },
+      { date: '2023-06-15', price    { date: '2023-07-01', price: 72 },
+      { date: '2023-07-15', price: 74 },
+      { date: '2023-08-01', price: 71 },
+      { date: '2023-08-15', price: 73 }
+    ],
+    'COOL008': [
+      { date: '2023-06-01', price: 50 },
+      { dat    { date: '2023-07-01', price: 52 },
+      { date: '2023-07-15', price: 54 },
+      { date: '2023-08-01', price: 51 },
+      { date: '2023-08-15', price: 53 }
+    ],
+    'NIC009': [
+      { date: '2023-06-01', price: 30 },
+      { date: '2023-06-15', price: 32 },
+      { date: '2023-07-01', price: 29 },
+      { date: '2023-07-15', price: 31 },
+      { date: '2023-08-01', price: 28 },
+      { date: '2023-08-15', price: 30 }
+    ]
+  };
 
 // Serve the index.html file
 app.get('/', (req, res) => {
